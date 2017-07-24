62ee0b28d3715338baf9038a5a5b31b96106ca9f
sam <bird_studio@163.com>
2017-07-24 10:19:10 +0800

    commit domain class

---
+++ src/main/java/cn/birdstudio/domain/Transaction.java
@@ -0,0 +1,62 @@
+package cn.birdstudio.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Table(name = "transaction", schema = "transaction")
+@Entity
+public class Transaction implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@SequenceGenerator(name = "transaction_generator", sequenceName = "transaction_sequence", initialValue = 1)
+	@GeneratedValue(generator = "transaction_generator")
+	private Long xid;
+
+	@Column(nullable = false)
+	private int seller_id;
+
+	@Column(nullable = false)
+	private int buyer_id;
+
+	@Column(nullable = false)
+	private int amount;
+
+	public Long getXid() {
+		return xid;
+	}
+
+	public void setXid(Long xid) {
+		this.xid = xid;
+	}
+
+	public int getSeller_id() {
+		return seller_id;
+	}
+
+	public void setSeller_id(int seller_id) {
+		this.seller_id = seller_id;
+	}
+
+	public int getBuyer_id() {
+		return buyer_id;
+	}
+
+	public void setBuyer_id(int buyer_id) {
+		this.buyer_id = buyer_id;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+}
---
+++ src/main/java/cn/birdstudio/domain/User.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2012-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.birdstudio.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Table(name = "user", schema = "user")
+@Entity
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@SequenceGenerator(name = "user_generator", sequenceName = "user_sequence", initialValue = 1)
+	@GeneratedValue(generator = "user_generator")
+	private Long id;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(nullable = false)
+	private int amt_sold;
+
+	@Column(nullable = false)
+	private int amt_bought;
+
+	public User() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAmt_sold() {
+		return amt_sold;
+	}
+
+	public void setAmt_sold(int amt_sold) {
+		this.amt_sold = amt_sold;
+	}
+
+	public int getAmt_bought() {
+		return amt_bought;
+	}
+
+	public void setAmt_bought(int amt_bought) {
+		this.amt_bought = amt_bought;
+	}
+
+	@Override
+	public String toString() {
+		return getName();
+	}
+}
